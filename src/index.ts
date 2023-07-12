48b240d2596d23acdd0d2017ce79453241fd4945
irakevine <ikevine2016@gmail.com>
2023-07-12 13:56:04 +0200

    Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-let sales = 2233_5454_45
-let course = 'typescript'
-let is_Published = true
-let level;
-function render(document){
-    console.log(document)
-}
-
+let numbers : number[]=[]
+    numbers.forEach (n=>n)
+  
 
