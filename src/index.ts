85f74f11b16b392aec07e4c6db837b4bdd245d5f
irakevine <ikevine2016@gmail.com>
2023-07-12 14:36:32 +0200

    introductions to function and some examples

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-const enum Size {Small= 1, Medium, Large}
-let mysize : Size = Size.Medium
-console.log(mysize);
+function CalculaterTax(income:number , TaxtYear: number):number{
+    
+    if(TaxtYear < 2022  )
+    return income*1.2;
+    return income*1.3;
+
+    
+}
+CalculaterTax(10_000, 2022)
