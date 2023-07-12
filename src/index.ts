ae0eba60400ce6352668f79341db27b8994d6203
irakevine <ikevine2016@gmail.com>
2023-07-12 14:53:30 +0200

    adding some object examples

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,20 @@ function CalculaterTax(income:number , TaxtYear: number):number{
     if(TaxtYear < 2022  )
     return income*1.2;
     return income*1.3;
-
-    
 }
 CalculaterTax(10_000, 2022)
+
+
+//OBJECTS
+ let Employee :{
+   readonly id:number,
+    name:string,
+    retire:(date:Date)=> void
+}= {
+    id:1, 
+    name:"Kellen",
+    retire:(date:Date) => {  
+    console.log(date)
+}
+    
+ }
