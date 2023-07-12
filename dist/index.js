4a85c8d0f8632d0d1822958bd47e2fa673b74070
irakevine <ikevine2016@gmail.com>
2023-07-12 15:19:28 +0200

    adding union Types

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
 "use strict";
-let mysize = 2;
-console.log(mysize);
+function KgTLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.22;
+    else
+        return parseInt(weight) * 2.22;
+}
+KgTLbs(10);
+KgTLbs('30kg');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
-function CalculaterTax(income:number , TaxtYear: number):number{
+// function CalculaterTax(income:number , TaxtYear: number):number{
     
-    if(TaxtYear < 2022  )
-    return income*1.2;
-    return income*1.3;
-}
-CalculaterTax(10_000, 2022)
+//     if(TaxtYear < 2022  )
+//     return income*1.2;
+//     return income*1.3;
+// }
+// CalculaterTax(10_000, 2022)
 
 
-//OBJECTS
-type employee ={
-    readonly id:number,
-     name:string,
-     retire:(date:Date)=> void
+// //OBJECTS
+// type employee ={
+//     readonly id:number,
+//      name:string,
+//      retire:(date:Date)=> void
 
-}
- let Employee : employee ={
-    id:1, 
-    name:"Kellen",
-    retire:(date:Date) => {  
-    console.log(date)
-}
+// }
+//  let Employee : employee ={
+//     id:1, 
+//     name:"Kellen",
+//     retire:(date:Date) => {  
+//     console.log(date)
+// }
     
- }
+//  }
+
+function KgTLbs(weight:number| string): number {
+    if (typeof weight === 'number' )
+    return weight * 2.22
+else 
+ return parseInt(weight)*2.22
+}
+ KgTLbs(10)
+ KgTLbs('30kg')
