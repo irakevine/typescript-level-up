b88afded1080d0035697062231eaaf1fe90df4c0
irakevine <ikevine2016@gmail.com>
2023-07-14 20:30:19 +0200

    adding classes type

--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, "buy ").concat(this.details, " and pays  ").concat(this.amount);
+    };
+    return Invoice;
+}());
+var invONe = new Invoice('kevine', 'works on the single react page with kane', 500);
+var invTwo = new Invoice('kellen', 'works on the single react page with ranedy', 200);
+var Invoices = [];
+Invoices.push(invONe);
+Invoices.push(invTwo);
+console.log(Invoices);
 var form = document.querySelector('.new-item-form');
 var field = document.querySelector('#type');
 var toFrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
+class Invoice {
+    client : string;
+    details:string;
+    amount: number
+constructor(c:string,d:string, a:number){
+    this.client =c
+    this.details= d
+    this.amount= a
+}
+format(){
+    return `${this.client}buy ${this.details} and pays  ${this.amount}`
+}
+
+}
+const invONe = new Invoice('kevine','works on the single react page with kane', 500)
+const invTwo = new Invoice('kellen','works on the single react page with ranedy', 200)
+let Invoices: Invoice[]=[]
+Invoices.push(invONe)
+Invoices.push(invTwo)
+
+console.log(Invoices);
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement
-
 const field = document.querySelector('#type') as HTMLSelectElement
-
 const toFrom= document.querySelector('#tofrom') as HTMLInputElement
-
 const details = document.querySelector('#details') as HTMLInputElement
-
 const Amount = document.querySelector('#amount') as HTMLInputElement
 form.addEventListener('submit',(e: Event ) =>{
     e.preventDefault();
