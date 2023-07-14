871331d1ac60afdbd5fe1d9840ad1e8e4e2988d0
irakevine <ikevine2016@gmail.com>
2023-07-14 18:16:06 +0200

    compling typeScript

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function getCustomer(id) {
-    return id === 0 ? null : { birtday: new Date() };
-}
-let Customer = getCustomer(1);
-console.log(Customer === null || Customer === void 0 ? void 0 : Customer.birtday);
-//# sourceMappingURL=index.js.map
